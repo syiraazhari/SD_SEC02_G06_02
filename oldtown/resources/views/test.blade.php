cf674db6148188a6792cef729bb83a9daefe53ec
adam imran <adamimran812@gmail.com>
2022-08-17 22:40:06 +0800

    first commit
    installed laravel and tailwindcss

---
+++ oldtown/resources/views/test.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <h1 class="text-emerald-500">Hello World</h1>
+    <ul>
+        <li>Composer</li>
+        <li>Laravel</li>
+        <li>NodeJs</li>
+    </ul>
+</body>
+</html>
