6ad3f8545c1c7a33be6f7e03ace7025cc50c216b
adam imran <adamimran812@gmail.com>
2022-10-20 21:13:59 +0800

    changed add menu

--- oldtown/resources/views/admin/menu/menus/add-menu.blade.php
+++ oldtown/resources/views/admin/menu/menus/add-menu.blade.php
@@ -84,7 +84,7 @@
 
                             <div class="my-2">
                                 <label class="text-secondary " for="description">Description</label>
-                                <textarea name="description" id="description"
+                                <textarea name="description" id="description" maxlength="70"
                                     class="block w-full px-4 py-2 mt-2 font-roboto text-slate-800 bg-white border border-slate-800 rounded-md focus:border-slate-800 focus:ring-slate-800  focus:ring-opacity-80 focus:outline-none focus:ring"
                                     style="resize: none;" required></textarea>
                             </div>
@@ -100,8 +100,6 @@
                                 </button>
                             </div>
                 </form>
-            </div>
-            </div>
             </div>
         </x-flex-view>
 
